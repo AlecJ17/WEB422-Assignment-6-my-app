f2b4cb3cc469a526d023c52cec51ab9940746270
alecq031723 <144410466+alecq031723@users.noreply.github.com>
2025-05-21 18:21:17 -0400

    add my-app files

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+    import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { useAtom } from "jotai";
